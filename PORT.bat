7fa2c17c1feb5d6a7b239a5e625a657ad4c6a457
RadioActive <davideinstein24@gmail.com>
2016-07-12 23:35:50 +0530

    Update PORT.bat

--- PORT.bat
+++ PORT.bat
@@ -1,14 +1,14 @@
 @echo off
 color 0b
-title Rom Porter by KINGbabasula
+title ROM Porter by Pavitra :)
 mkdir Base-Rom
 mkdir Port-Rom
 cls
 echo.
 echo   ****************************************************************************
 echo   *                                                                          *
-echo   *                             Rom Porter                                   *
-echo   *                           by KINGbabasula                                *
+echo   *                             ROM Porter                                   *
+echo   *                            by Pavitra :)                                 *
 echo   *                                                                          *
 echo   ****************************************************************************
 echo.
@@ -19,26 +19,33 @@ goto start
 cls
 echo.
 echo  =============================================================================
-echo                      Welcome to KINGbabasula Rom Porter
+echo                      Welcome to Pavitra ROM Porter
 echo.
-echo    What do you need?
-echo    1.A clean Cyanogenmod or Aosp that is running on your device as Base-Rom
-echo    2.The rom you want to port as Port-Rom
+echo    Things that you will need:
+echo.
+echo    1.A clean Cyanogenmod or AOSP that is running on your device as Base-Rom
+echo    2.The ROM you want to port as Port-Rom
 echo.
 pause
 cls
 echo.
+echo                                  Menu
+echo.
 echo    What rom are you porting?
+echo.
 echo    1.Stock or Cyanogenmod based
 echo    2.MIUI
 echo    3.Paranoid Android
 echo    4.PAC MAN
 echo    5.Manufacturer rom (Touchwiz, Sense)
 echo    6.Lewa OS
+echo. 
+echo                    Troubleshoot
+echo.
 echo    7.Run fix tool
 echo    8.Exit
 echo  =============================================================================
-set /p sdffs=Enter your decision:
+set /p sdffs=Enter your choice:
 if %sdffs%==1 (start sto.bat)
 if %sdffs%==2 (start miui.bat)
 if %sdffs%==3 (start pa.bat)
